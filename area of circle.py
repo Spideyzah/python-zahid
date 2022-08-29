5e0b08ec4551ef4de7923d11170c89d522e31d07
zahid ahmed <107020524+Spideyzah@users.noreply.github.com>
2022-08-29 14:14:06 +0530

    Update area of circle.py

--- area of circle.py
+++ area of circle.py
@@ -1,3 +1,3 @@
-r=int(input("enter the radius of the circle:"))
+r=int(input("enter the number of radius of the circle:"))
 area_of_circle=3.14*r*r
 print("the area of the circle is:",area_of_circle)
